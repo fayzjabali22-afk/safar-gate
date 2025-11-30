fedcc43c576a5d458be04179bb36b5fb05ba8606
Fayz Al jabali <alexeyb231@gmail.com>
2025-11-30 02:23:21 +0000

    قوم بحذف اللوجو

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -4,15 +4,10 @@ export function Logo({ className }: { className?: string }) {
   return (
     <div
       className={cn(
-        'flex items-center justify-center gap-2 text-lg font-bold tracking-tighter text-primary p-2',
+        'flex items-center justify-center gap-2 text-lg font-bold tracking-tighter text-primary p-2 h-[56px]',
         className
       )}
     >
-      <img
-        src="https://i.postimg.cc/zvbhTsXV/Iwjw-sfryat.png"
-        alt="Safar Carrier Logo"
-        style={{ width: '140px', height: '40px', objectFit: 'contain' }}
-      />
     </div>
   );
 }
