bab610adf29423a4cfca8c5b6c37c38ee449c5b6
Fayz Al jabali <alexeyb231@gmail.com>
2025-12-07 05:58:40 +0000

    Try fixing this error: `Build Error: Export useUserProfile doesn't exist

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -50,6 +50,8 @@ export * from './non-blocking-updates';
 export * from './non-blocking-login';
 export * from './errors';
 export * from './error-emitter';
+export * from '@/hooks/use-user-profile';
+
 
 // This function needs to be explicitly typed for use in components
 export function useAuth(): Auth | null {
