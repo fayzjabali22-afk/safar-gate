222e4c73dc70b91688ac8c17b47e9f3e77b9dc27
Fayz Al jabali <alexeyb231@gmail.com>
2025-12-07 02:30:05 +0000

    ؟

--- src/components/carrier/carrier-bottom-nav.tsx
+++ src/components/carrier/carrier-bottom-nav.tsx
@@ -23,7 +23,7 @@ export function CarrierBottomNav({ onAddTripClick }: CarrierBottomNavProps) {
   const pathname = usePathname();
 
   return (
-    <div className="md:hidden fixed bottom-0 left-0 right-0 h-20 bg-card border-t shadow-[0_-4px_12px_rgba(0,0,0,0.1)] z-50 pb-[env(safe-area-inset-bottom)]">
+    <div className="carrier-bottom-nav">
       <div className="relative h-full">
         {/* Floating Action Button for Add Trip */}
         <div className="absolute left-1/2 -top-7 -translate-x-1/2 flex items-center justify-center">
