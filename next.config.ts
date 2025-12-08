11e6443dc9becfc1b39e011ed360bf8303856b31
Fayz Al jabali <alexeyb231@gmail.com>
2025-12-08 07:35:46 +0000

    Try fixing this error: `Runtime Error: Error: Invalid src prop (https://

--- next.config.ts
+++ next.config.ts
@@ -7,7 +7,8 @@ const nextConfig: NextConfig = {
     // هذه الإعدادات تسمح بجلب الصور من مصادر خارجية
     remotePatterns: [
       { protocol: 'https', hostname: 'images.unsplash.com' },
-      { protocol: 'https', hostname: 'firebasestorage.googleapis.com' } 
+      { protocol: 'https', hostname: 'firebasestorage.googleapis.com' },
+      { protocol: 'https', hostname: 'i.ibb.co' }
     ],
     // unoptimized: true, // يمكنك تفعيل هذا السطر إذا واجهت مشاكل في الصور لاحقاً
   },
